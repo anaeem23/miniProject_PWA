ba3a1576f44aaf953f8f5616d5710f87034a4e71
Spider-Man <abeernaeem6@gmail.com>
2022-06-07 20:48:35 -0500

    initial commit

---
+++ client/webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { GenerateSW } = require('workbox-webpack-plugin');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
+    },
+
+    // TODO: Add the correct output
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+
+      new GenerateSW(),
+      new WebpackPwaManifest({
+          "name": "My Progressive Web App",
+          "orientation": "portrait",
+          "display": "standalone",
+          "start_url": "./",
+          "short_name": "MyPWA",
+          "description": "My awesome Progressive Web App!",
+          "background_color": "#ffffff",
+          "icons": 
+            {
+              "src": "src/images/logo.png",
+              "sizes": "1024x1024",
+              "type": "image/png",
+              "purpose": "maskable"
+            }
+      }),
+     
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
+
